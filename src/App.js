a88baa26dc45883bb3d7012b17db78c8f8c6120c
jm <mmmobl@naver.com>
2023-01-26 19:38:05 +0900

    230126 19:38 jm

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { Fragment } from 'react';
 import Header from './layout/Header';
 import Section from './layout/Section';
 import Article from './layout/Article';
+import { Route, Routes } from 'react-router-dom';
+import Reservation from './layout/Reservation';
 
 function App() {
 
 
   return (
-    <Fragment>
-      <Header />
-      <Section />
-      <Article />
-    </Fragment>
+    <Routes>
+      <Route path='/' element={<Header />}>
+        <Route path='/' element={<Section />}>
+          <Route path='/' element={<Article />} />
+        </Route>
+        <Route path='/reservation' element={<Reservation />} />
+      </Route>
+    </Routes>
   );
 }
 export default App;
